7883573b3f98a005f6706a6702c98169fa463f89
Genís Picó Cordo <genis.pico@hotmail.com>
2025-01-13 14:32:14 +0100

    Update first_word.c

--- level 1/first_word/first_word.c
+++ level 1/first_word/first_word.c
@@ -12,21 +12,25 @@
 
 #include <unistd.h>
 
+int	is_space(char c)
+{
+	return (c == ' ' || c >= 9 && c <= 13);
+}
+
 int	main(int argc, char **argv)
 {
 	int	i;
 
+	i = 0;
 	if (argc == 2)
 	{
-		i = 0;
-		while (argv[1][i] == 32 || argv[1][i] == 9)
+		while(is_space(argv[1][i]))
 			i++;
-		while ((argv[1][i] != 32 && argv[1][i] != 9) && argv[1][i])
+		while (argv[1][i] && !is_space(argv[1][i]))
 		{
 			write(1, &argv[1][i], 1);
 			i++;
 		}
 	}
 	write(1, "\n", 1);
-	return (0);
 }
