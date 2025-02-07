e1a3c421fa1b328e7de503d6efdf5062ac91f36e
Genís Picó Cordo <genis.pico@hotmail.com>
2025-02-07 11:38:14 +0100

    Update search_and_replace.c

--- level 1/search_and_replace/search_and_replace.c
+++ level 1/search_and_replace/search_and_replace.c
@@ -13,45 +13,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int	check_char(char *str, char c)
+int main(int argc, char **argv)
 {
-	int	i;
+	int i;
+	char *str;
 
 	i = 0;
-	while (str[i])
-	{
-		if (str[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	main(int argc, char **argv)
-{
-	int		i;
-	char	*str;
-
 	str = argv[1];
-	i = 0;
-	if ((argc != 4) || argv[2][1] || argv[3][1])
+	if((argc != 4) || argv[2][1] || argv[3][1])
 	{
 		write(1, "\n", 1);
-		exit(1);
+		exit (1);
 	}
-	else if (!check_char(argv[1], argv[2][0]))
-		while (str[i])
-			write(1, &str[i++], 1);
 	else
 	{
-		while (str[i])
+		while(str[i])
 		{
-			if (str[i] == argv[2][0])
-				write (1, &argv[3][0], 1);
+			if(str[i] == argv[2][0])
+				write(1, &argv[3][0], 1);
 			else
-				write (1, &str[i], 1);
+				write(1, &str[i], 1);
 			i++;
 		}
-	}
 	write(1, "\n", 1);
+	}
 }
