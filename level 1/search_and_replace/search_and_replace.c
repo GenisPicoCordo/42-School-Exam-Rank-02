df104aaaec244a32faeb886c6bd5b4ee67936074
Genís Picó Cordo <genis.pico@hotmail.com>
2025-02-12 08:39:14 +0100

    Update search_and_replace.c

--- level 1/search_and_replace/search_and_replace.c
+++ level 1/search_and_replace/search_and_replace.c
@@ -11,25 +11,28 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-	int i;
-	char *str;
+    int i;
+    char *str;
 
-	i = 0;
-	str = argv[1];
-	if((argc == 4) || !argv[2][1] || !argv[3][1])
-	{
-		while(str[i])
-		{
-			if(str[i] == argv[2][0])
-				write(1, &argv[3][0], 1);
-			else
-				write(1, &str[i], 1);
-			i++;
-		}
-	}
-	write(1, "\n", 1);
+    if (argc != 4 || argv[2][1] != '\0' || argv[3][1] != '\0')
+    {
+        write(1, "\n", 1);
+        return 0;
+    }
+
+    str = argv[1];
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == argv[2][0])
+            write(1, &argv[3][0], 1);
+        else
+            write(1, &str[i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+    return 0;
 }
