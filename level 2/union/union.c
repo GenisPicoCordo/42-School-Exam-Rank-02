9067a3bca5a6ec3f59cabb30072417e7081e3446
Genís Picó Cordo <genis.pico@hotmail.com>
2025-02-12 19:51:23 +0100

    Update union.c

--- level 2/union/union.c
+++ level 2/union/union.c
@@ -12,52 +12,47 @@
 
 #include <unistd.h>
 
-int	already_seen(char *printed, char c, int len)
+int	ft_strchr(char *abc, char c)
 {
-	int	i = 0;
-	while (i < len)
+	int	i;
+
+	i = 0;
+	while (abc[i])
 	{
-		if (printed[i] == c)
+		if (abc[i] == c)
 			return (1);
 		i++;
 	}
 	return (0);
 }
 
-void	print_union(char *s1, char *s2)
+void	process_string(char *str, char abc[26], int *j)
 {
-	char	seen[256];
-	int		count = 0;
-	int		i = 0;
+	int	i;
 
-	
-	while (s1[i])
-	{
-		if (!already_seen(seen, s1[i], count))
-		{
-			seen[count++] = s1[i];
-			write(1, &s1[i], 1);
-		}
-		i++;
-	}
 	i = 0;
-	while (s2[i])
+	while (str[i])
 	{
-		if (!already_seen(seen, s2[i], count))
+		if (!ft_strchr(abc, str[i]))
 		{
-			seen[count++] = s2[i];
-			write(1, &s2[i], 1);
+			*j += 1;
+			abc[*j] = str[i];
+			write(1, &str[i], 1);
 		}
 		i++;
 	}
-	write(1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
 {
+	char	abc[26];
+	int		j;
+
 	if (argc == 3)
-		print_union(argv[1], argv[2]);
-	else
-		write(1, "\n", 1);
-	return (0);
+	{
+		j = -1;
+		process_string(argv[1], abc, &j);
+		process_string(argv[2], abc, &j);
+	}
+	write(1, "\n", 1);
 }
