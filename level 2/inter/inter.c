c7408dd30cb1bac4aabdd4aa234d54feb11936c6
Genís Picó Cordo <genis.pico@hotmail.com>
2025-02-14 15:13:27 +0100

    Update inter.c

--- level 2/inter/inter.c
+++ level 2/inter/inter.c
@@ -12,49 +12,43 @@
 
 #include <unistd.h>
 
-int	search(char c, char *abc)
+int ft_strchr(const char *str, char c)
 {
-	int	i;
+    int i;
 
-	i = 0;
-	while (abc[i])
-	{
-		if (abc[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
+    i = 0;
+    while(str[i])
+    {
+        if(str[i] == c)
+            return(1);
+        i++;
+    }
+    return(0);
 }
 
-void	process_print(char *abc, char c, int *count)
+int main(int argc, char **argv)
 {
-	abc[*count] = c;
-	*count += 1;
-	write(1, &c, 1);
-}
+    int i;
+    char abc[26];
+    int pos;
 
-int	main(int argc, char **argv)
-{
-	int		i;
-	int		j;
-	int		count;
-	char	abc[26];
-
-	if (argc == 3)
-	{
-		i = 0;
-		count = 0;
-		while (argv[1][i])
-		{
-			j = 0;
-			while (argv[2][j])
-			{
-				if (argv[1][i] == argv[2][j] && !search(argv[1][i], abc))
-					process_print(abc, argv[1][i], &count);
-				j++;
-			}
-			i++;
-		}
-	}
-	write (1, "\n", 1);
+    i = 0;
+    pos = 0;
+    if(argc == 3)
+    {
+        while(argv[1][i])
+        {
+            if(ft_strchr(argv[2], argv[1][i]))
+            {
+                if(!ft_strchr(abc, argv[1][i]))
+                {
+                    abc[pos] = argv[1][i];
+                    pos += 1;
+                    write(1, &argv[1][i], 1);
+                }
+            }
+            i++;
+        }
+    }
+    write(1, "\n", 1);
 }
