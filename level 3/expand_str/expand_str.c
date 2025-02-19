363f5d0e82ce60811a77dbac45501149b32ccf92
Genís Picó Cordo <genis.pico@hotmail.com>
2025-02-19 11:17:24 +0100

    Update expand_str.c

--- level 3/expand_str/expand_str.c
+++ level 3/expand_str/expand_str.c
@@ -12,37 +12,39 @@
 
 #include <unistd.h>
 
-int	is_space(char c)
+int is_space(char c)
 {
-	if (c == ' ' || c == 9)
-		return (1);
-	return (0);
+    return(c == ' ' || (c >= 9 && c <= 13));
 }
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int	i;
-	int	in_space;
+    int i = 0;
+    int in_word = 0;
 
-	if (argc == 2)
-	{
-		i = 0;
-		in_space = 0;
-		while (is_space(argv[1][i]))
-			i++;
-		while (argv[1][i])
-		{
-			if (is_space(argv[1][i]))
-				in_space = 1;
-			if (!is_space(argv[1][i]))
-			{
-				if (in_space)
-					write(1, "   ", 3);
-				in_space = 0;
-				write(1, &argv[1][i], 1);
-			}
-			i++;
-		}
-	}
-	write(1, "\n", 1);
+    if(argc == 2)
+    {
+        while(is_space(argv[1][i]))
+            i++;
+        while(argv[1][i])
+        {
+            if(!is_space(argv[1][i]))
+            {
+                if(in_word)
+                    write(1, "   ", 1);
+                while(argv[1][i] && !is_space(argv[1][i]))
+                {
+                    write(1, &argv[1][i], 1);
+                    i++;
+                }
+                in_word = 1;
+            }
+            else    
+            {
+                while(argv[1][i] && is_space(argv[1][i]))
+                    i++;
+            }
+        }
+    }
+    write(1, "\n", 1);
 }
