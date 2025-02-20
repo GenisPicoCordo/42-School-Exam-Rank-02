aae2eeefbd1dc51fc82d4898913edfbac9542bd3
Genís Picó Cordo <genis.pico@hotmail.com>
2025-02-20 11:07:48 +0100

    Update ft_list_remove_if.c

--- level 4/ft_list_remove_if/ft_list_remove_if.c
+++ level 4/ft_list_remove_if/ft_list_remove_if.c
@@ -19,13 +19,15 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 
     if(*begin_list == NULL)
         return ;
+    tmp = *begin_list;
     if((cmp)(tmp->data, data_ref) == 0)
     {
-        tmp = *begin_list;
-        *begin_list = (*begin_list)->next;
+        *begin_list = *tmp->next;
         free(tmp);
         ft_list_remove_if(begin_list, data_ref, cmp);
     }
     else
+    {
         ft_list_remove_if(&((*begin_list)->next), data_ref, cmp);
+    }
 }
